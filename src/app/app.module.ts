e16950a765820fa711657896a99893ef3ee0a897
QuentinNguyen <quentin.nguyen@edu.ece.fr>
2022-04-22 14:53:08 +0200

    Amelioration de l'architecture du code (Core/ Features)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,36 +1,31 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
-import { FaceSnapComponent } from './face-snap/face-snap.component';
-import { FaceSnapListComponent } from './face-snap-list/face-snap-list.component';
-import { HeaderComponent } from './header/header.component';
+import { FaceSnapComponent } from './face-snaps/components/face-snap/face-snap.component';
+
+import { HeaderComponent } from './core/component/header/header.component';
 import { AppRoutingModule } from './app-routing.module';
-import { LandingPageComponent } from './landing-page/landing-page.component';
-import { SingleFaceSnapComponent } from './single-face-snap/single-face-snap.component';
+import { LandingPageComponent } from './landing-page/components/landing-page.component';
+
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { NewFaceSnapComponent } from './new-face-snap/new-face-snap.component';
+
 import {HttpClientModule, HTTP_INTERCEPTORS} from '@angular/common/http'
-import { HttpInterceptorProviders } from './interceptors';
+import { HttpInterceptorProviders } from './core/interceptors';
+import { CoreModule } from './core/core.module';
+import { FaceSnapsModule } from './face-snaps/face-snaps.module';
+import { LandingPageModule } from './landing-page/landing-page.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    FaceSnapComponent,
-    FaceSnapListComponent,
-    HeaderComponent,
-    LandingPageComponent,
-    SingleFaceSnapComponent,
-    NewFaceSnapComponent,
+
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule,
-    ReactiveFormsModule,
-    HttpClientModule
-  ],
-  providers: [
-    HttpInterceptorProviders
+    CoreModule,
+    FaceSnapsModule,
+    LandingPageModule
   ],
   bootstrap: [AppComponent]
 })
---
+++ src/app/core/core.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HttpInterceptorProviders } from './interceptors';
+import { HeaderComponent } from './component/header/header.component';
+import { RouterModule } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
+
+
+
+@NgModule({
+  declarations: [
+    HeaderComponent
+  ],
+  imports: [
+    CommonModule,
+    RouterModule,
+    HttpClientModule
+  ], 
+  exports:[
+    HeaderComponent
+  ],
+  providers: [
+    HttpInterceptorProviders
+  ]
+})
+export class CoreModule { }
